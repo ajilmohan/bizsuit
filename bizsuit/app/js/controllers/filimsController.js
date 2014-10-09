9f5ea5a64872dc785f5f9254199477ed26d57394
unknown <ajilam@NIBC2052.nousinfo.com>
2014-10-09 12:24:20 +0530

    Filim details updated

--- bizsuit/app/js/controllers/filimsController.js
+++ bizsuit/app/js/controllers/filimsController.js
@@ -18,14 +18,15 @@ angular.module('filims')
 			$scope.filims = filims;
 				var index = 0;
 			 	
-
+				 $scope.filimToDisplay = $scope.filims[0];
 			    $interval(function(){
 					 $scope.filimToDisplay = $scope.filims[index];
 					index++;
 					if (index == $scope.filims.length - 1 ) {
+						$scope.filimToDisplay = $scope.filims[index];
 						index = 0;
 					};
-				},10000);
+				},7000);
 
 			 
 			
