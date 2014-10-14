43a72bbf5c4e548e7fb4407d41c674d039e467eb
ajilmohan <ajilam@gmail.com>
2014-10-14 09:56:16 +0530

    Bizsuit admin console add image commit

--- bizsuit-admin/app/js/controllers/filimsController.js
+++ bizsuit-admin/app/js/controllers/filimsController.js
@@ -16,22 +16,36 @@ angular.module('filims')
 		filimDb.getFilims(function(filims){
 			
 			$scope.filims = filims;
-				var index = 0;
+			var index = 0;
 			 	
-				 $scope.filimToDisplay = $scope.filims[0];
-			    $interval(function(){
-					 $scope.filimToDisplay = $scope.filims[index];
-					index++;
-					if (index  $scope.filims.length - 1 ) {
-						debugger;
-						//$scope.filimToDisplay = $scope.filims[index];
-						index = 0;
-					};
-				},7000);
+			$scope.filimToDisplay = $scope.filims[0];
+			recursiveCall(index);
 
 			 
 			
 		});
+
+
+		function recursiveCall(index){
+			debugger;
+
+			 $interval(function(){
+					 $scope.filimToDisplay = $scope.filims[index];
+					
+					/*if (index  $scope.filims.length - 1 ) {
+						debugger;
+						//$scope.filimToDisplay = $scope.filims[index];
+						index = 0;
+					};*/
+				},7000);
+			 index++;
+
+			 if(index == $scope.filims.length  ) {
+			 	index = 0;
+			 }
+			 recursiveCall(index);
+
+		}
 
 		
 
--- bizsuit/app/js/controllers/filimsController.js
+++ bizsuit/app/js/controllers/filimsController.js
@@ -17,21 +17,34 @@ angular.module('filims')
 			
 			$scope.filims = filims;
 				var index = 0;
-			 	
+			 	var reset  = false;
 				 $scope.filimToDisplay = $scope.filims[0];
 			    $interval(function(){
+
 					 $scope.filimToDisplay = $scope.filims[index];
-					index++;
+					
 					if (index == $scope.filims.length - 1 ) {
 
 						$scope.filimToDisplay = $scope.filims[index];
 						index = 0;
+						reset  = true;
 					};
+					if(!reset){
+						index++;
+					}
+
+					if (reset) {
+						reset = false;
+					};
+					
 				},7000);
 
 			 
 			
 		});
+
+
+
 
 
 	});
